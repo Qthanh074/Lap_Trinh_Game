05aaaabe3c1a88ebc738a1d4799ee70728e33976
Qthanh <daoquangthanh0704@gmail.com>
2026-01-19 00:06:46 +0700

    Nop bai Space Shooter Final

---
+++ Assets/Blinking.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class Blinking : MonoBehaviour
+{
+    private SpriteRenderer spriteRenderer;
+
+    void Start()
+    {
+        // Lấy component SpriteRenderer khi bắt đầu
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    void Update()
+    {
+        // Bật/tắt renderer liên tục mỗi khung hình để tạo hiệu ứng nhấp nháy
+        spriteRenderer.enabled = !spriteRenderer.enabled;
+    }
+}
---
+++ Assets/showlog.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using System.Collections;
+
+public class ShowLog : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Hello World!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("Update called! " + Time.frameCount);
+    }
+}
